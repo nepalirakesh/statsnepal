375a721ab033bbac1788c0931e5b4a69335f19e5
nepalirakesh <kinarisako@gmail.com>
2022-12-29 06:30:38 +0545

    new branch for adhoc query testing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SurveyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,4 @@ Route::get('/check',function(){
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/survey',[SurveyController::class,'survey']);
