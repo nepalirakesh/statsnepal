4b8d6919081e124147b4c86a6d8be7e237e0966d
nepalirakesh <kinarisako@gmail.com>
2023-01-02 18:02:15 +0545

    custom validation using formrequest,flash error message,read/update of form

---
+++ app/Http/Requests/PostFormRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return[
+            'title'=>'required|alpha',
+            'content'=>'required|max:200'
+        ];
+    }
+
+
+
+
+    public function messages()
+{
+    return [
+        'title.required' => 'A title is required',
+        'title.alpha'=>'Only alphabaets allowed',
+        'content.required' => 'A message is required',
+        'content.max'=>'only 200 characters allowe',
+    ];
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::get('/posts',[PostController::class,'index'])->name('posts');
 Route::get('/posts/create',[PostController::class,'create'])->name('create');
 Route::post('/posts/store',[PostController::class,'store'])->name('store');
 Route::delete('/posts/delete/{post}',[PostController::class,'delete'])->name('delete');
+Route::get('/posts/edit/{post}',[PostController::class,'edit'])->name('edit');
+Route::put('/posts/update/{post}',[PostController::class,'update'])->name('update');
+Route::get('/posts/show/{post}',[PostController::class,'show'])->name('show');
